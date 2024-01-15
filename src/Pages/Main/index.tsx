8ea121b000adf7d58684e31973c6149740fc2693
ChooSeongWoo <sldoal2423@gmail.com>
2024-01-16 00:43:54 +0900

    ADD :: Main 퍼블리싱

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -4,14 +4,52 @@ import * as S from "./style";
 import Button from "../../components/Button";
 
 const Main = () => {
+  const multipleButtons = false;
   return (
     <S.Layout>
-      <Button
-        title="로그인"
-        onClickMethod={() => {
-          console.log(1);
-        }}
-      />
+      <S.Omok src="../../../img/MainBackground.svg" alt="바둑판" />
+      <S.Layout2 />
+      <S.Items multipleButtons={multipleButtons}>
+        <S.Icon src="../../../img/Icon.svg" alt="" />
+        <S.Title>오목킹</S.Title>
+        <S.Buttons>
+          {multipleButtons ? (
+            <>
+              <Button
+                title="게임참가"
+                onClickMethod={() => {
+                  console.log(1);
+                }}
+              />
+              <Button
+                title="게임생성"
+                onClickMethod={() => {
+                  console.log(1);
+                }}
+              />
+              <Button
+                title="마이페이지"
+                onClickMethod={() => {
+                  console.log(1);
+                }}
+              />
+              <Button
+                title="로그아웃"
+                onClickMethod={() => {
+                  console.log(1);
+                }}
+              />
+            </>
+          ) : (
+            <Button
+              title="로그인"
+              onClickMethod={() => {
+                console.log(1);
+              }}
+            />
+          )}
+        </S.Buttons>
+      </S.Items>
     </S.Layout>
   );
 };
--- src/Pages/Main/style.ts
+++ src/Pages/Main/style.ts
@@ -1,3 +1,51 @@
 import styled from "styled-components";
 
-export const Layout = styled.div``;
+export const Layout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #ffa71a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Omok = styled.img`
+  width: 90%;
+  height: 90%;
+  position: absolute;
+`;
+
+export const Layout2 = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background-color: rgba(217, 217, 217, 0.5);
+`;
+
+export const Items = styled.div<{ multipleButtons: boolean }>`
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: ${(props) => (props.multipleButtons ? "55vh" : "30vh")};
+  justify-content: space-between;
+  margin-bottom: ${(props) => (props.multipleButtons ? "" : "10%")};
+`;
+
+export const Icon = styled.img`
+  width: 6vw;
+`;
+
+export const Title = styled.p`
+  font-family: "Sejong Bold";
+  font-size: 40px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 10%;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
