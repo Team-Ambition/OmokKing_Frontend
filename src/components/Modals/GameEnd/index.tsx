1a062ff6a13392ce4e18f72d823b177d5445a58a
ChooSeongWoo <sldoal2423@gmail.com>
2024-01-18 23:40:15 +0900

    ADD :: 게임 종료 모달 퍼블리싱

--- src/components/Modals/GameEnd/index.tsx
+++ src/components/Modals/GameEnd/index.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import "./style";
+import * as S from "./style";
+import Button from "../../Button";
+import { useNavigate } from "react-router-dom";
 
 const GameEnd = () => {
-  return <div></div>;
+  const history = useNavigate();
+  
+  return (
+    <S.ModalOverlay>
+      <S.Layout>
+        <S.Profile>
+          <S.ProfileImg src="../../../../img/Profile.png" />
+          <S.ProfileName>추성우</S.ProfileName>
+        </S.Profile>
+        <S.End>
+          <S.EndImg
+            src={0 ? "../../../../img/Win.svg" : "../../../../img/Lose.svg"}
+          />
+          <S.EndTitle>{0 ? "Win!" : "Lose!"}</S.EndTitle>
+        </S.End>
+        <S.Button>
+          <Button
+            title="방나가기"
+            onClickMethod={() => {
+              history("/");
+            }}
+          />
+        </S.Button>
+      </S.Layout>
+    </S.ModalOverlay>
+  );
 };
 
 export default GameEnd;
--- src/components/Modals/GameEnd/style.ts
+++ src/components/Modals/GameEnd/style.ts
@@ -1,1 +1,65 @@
 import styled from "styled-components";
+
+export const ModalOverlay = styled.div`
+  z-index: 5;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Layout = styled.div`
+  width: 640px;
+  height: 300px;
+  background: #bd8154;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  position: relative;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 150px;
+`;
+
+export const ProfileImg = styled.img`
+  border-radius: 150px;
+  width: 120px;
+  height: 120px;
+`;
+
+export const ProfileName = styled.p`
+  font-family: "Sejong Bold";
+`;
+
+export const End = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const EndImg = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+export const EndTitle = styled.p`
+  font-size: 80px;
+  color: white;
+`;
+
+export const Button = styled.div`
+  position: absolute;
+  bottom: 3%;
+  right: 2%;
+`;
